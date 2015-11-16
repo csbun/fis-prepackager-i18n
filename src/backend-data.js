2e0b46ad3c97ff250824b1bf3fe4dec2b1b760c1
Hans Chan <chenhc@ucweb.com>
2015-11-16 16:34:26 +0800

    add backend-data

---
+++ src/backend-data.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/**
+ * 替换成 play 参数
+ */
+function replacerPlayArg (bd) {
+    return '${' + bd.name + '}';
+}
+
+/**
+ * 替换成 backend-data.json 的内容
+ */
+function replacerData(bd) {
+    if (typeof bd.data === 'string') {
+        return bd.data;
+    } else {
+        return JSON.stringify(bd.data, null, 4);
+    }
+}
+
+module.exports = function (file, fisOpt) {
+    var backendArgs = [];
+    var content = file.getContent();
+
+    // 如果 -p 打包，则转义 play 特殊字符
+    if (fisOpt.pack) {
+        content = content.replace(/(`|~|@\{|\$\{|%\{)/g, '~$1');
+    }
+
+    try {
+        // 强制删除 require(backend-data.js) 的 cache
+        var backendDataFile = file.dirname + '/backend-data.js';
+        if (require.cache[backendDataFile]) {
+            delete require.cache[backendDataFile];
+        }
+        // 监听文件变化依赖
+        file.cache.addDeps(backendDataFile);
+        // 如果 -p 打包，则替换成 play 参数
+        // 否则替换成 backend-data.json 的内容
+        var replacer = fisOpt.pack ? replacerPlayArg : replacerData;
+        // 遍历，替换
+        require(backendDataFile).map(function (bd) {
+            backendArgs.push('String ' + bd.name);
+            var reg = new RegExp('\\b' + bd.placeholder + '\\b', 'g');
+            content = content.replace(reg, function () {
+                return replacer(bd);
+            });
+        });
+        // 如果 -p 打包，添加全部 play 参数
+        if (fisOpt.pack && backendArgs.length) {
+            content = '`args ' + backendArgs.join(', ') + '\n' + content;
+        }
+    } catch (e) {
+        console.log(e);
+    }
+    file.setContent(content);
+};
--- src/build-components.js
+++ src/build-components.js
@@ -4,19 +4,15 @@ var UNIT_REG = /\{\{\{unit(?=\s)([^}]+)\}\}\}/g;
 var UNIT_NAME_REG = /\sname\s*=\s*"([^"]+)"/;
 var UNIT_DATA_REG = /\sdata\s*=\s*"([^"]+)"/;
 
-var BACKEND_DATA = fis.config.get('backend.data');
-var BACKEND_DATA_REG = BACKEND_DATA.reg;
-var BACKEND_DATA_IMPORT = '`args String ' + BACKEND_DATA.server + '\n';
-var BACKEND_DATA_ARG = '${' + BACKEND_DATA.server + '}';
-
 var wrapError = require('./wrap-error');
+var backendData = require('./backend-data');
 
 /**
  * 组装 components
  */
 module.exports = function (file, fisRet, fisSetting, fisOpt) {
     var content = file.getContent();
-    var needBackendImport = false;
+    // 组装 components
     content = content
         // 替换 Layout 中的 组件单元
         .replace(UNIT_REG, function (m) {
@@ -44,29 +40,11 @@ module.exports = function (file, fisRet, fisSetting, fisOpt) {
             }
             return m;
         });
-    // 如果 -p 打包，则转义 play 特殊字符
-    if (fisOpt.pack) {
-        content = content.replace(/(`|~|@\{|\$\{|%\{)/g, '~$1');
-    }
-    // 替换 BACKEND_DATA
-    content = content.replace(BACKEND_DATA_REG, function () {
-            if (fisOpt.pack) {
-                // 如果 -p 打包，则替换成 play 参数
-                needBackendImport = true;
-                return BACKEND_DATA_ARG;
-            } else {
-                // 否则替换成 backend-data.json 的内容
-                var mockBackendData = {};
-                var backendDataFilePath = file.subdirname + '/backend-data.json';
-                try {
-                    mockBackendData = JSON.parse(fisRet.src[backendDataFilePath].getContent());
-                } catch (e) {}
-                return JSON.stringify(mockBackendData, null, 4);
-            }
-        });
-    if (needBackendImport) {
-        content = BACKEND_DATA_IMPORT + content;
-    }
-    // 保存内容
     file.setContent(content);
+
+    // 替换 BACKEND_DATA
+    backendData(file, fisOpt);
+    
+    // 文件更新到 release 目录
+    fisOpt.beforeCompile(file);
 };
